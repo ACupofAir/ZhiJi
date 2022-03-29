544ec6b6e1ad74439a43b2686d465416de495ee1
Mitty-Guang <915004396@qq.com>
2022-03-29 20:05:52 +0800

    按钮功能没有实现

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import Vue from 'vue';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+//
+// Vue.use(ElementUI);
+//
+// new Vue({
+//     el: '#app',
+//     render: h => h(App)
+// });
+
 createApp(App).use(router).mount('#app')
 
